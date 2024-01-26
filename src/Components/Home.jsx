074f875610ff3adea703c435653c0563d28c11da
MUHAMMAD FAROOQ KHAN <75903910+mfarooqk47@users.noreply.github.com>
2024-01-26 16:53:35 +0500

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/mountains.jpg";
+import image from "../images/mountains.png";
 
-const imageAltText = "mountains backgrohnds photo";
+const imageAltText = "mountains background photo";
 
 const Home = ({ name, title }) => {
   return (
