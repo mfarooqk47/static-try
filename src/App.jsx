403da66ebebc0a8adf9f9043082607c0dd10c251
MUHAMMAD FAROOQ KHAN <75903910+mfarooqk47@users.noreply.github.com>
2024-01-26 16:35:00 +0500

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
+  name: "Muhammad Farooq Khan",
+  title: "Project Manager and Data Analyst",
+  email: "mfarooqk47@gmail.com",
+  gitHub: "mfarooqk47",
+  instagram: "itsmfarooq",
+  linkedIn: "https://www.linkedin.com/in/muhammadfarooqk/",
+  medium: "mfarooqk47",
+  twitter: "itsmfarooq",
   youTube: "Code",
 };
 
